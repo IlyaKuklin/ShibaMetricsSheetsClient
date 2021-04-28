609b179ec9185b4f9b917eaaa73bf622b04bc210
Ilya Kuklin <ilyakuklin43@gmail.com>
2021-04-28 14:37:50 +0300

    yd acc refresh fix

--- src/app/integration/services/sm-yandex-auth.service.ts
+++ src/app/integration/services/sm-yandex-auth.service.ts
@@ -16,10 +16,10 @@ export class SmYandexAuthService {
 	private readonly STORAGE_KEY: string = 'yapi_data';
 	private readonly AUTH_URL: string = 'https://oauth.yandex.ru';
 
-	isSignedInYandex(name: string): boolean {
+	isSignedInYandex(login: string): boolean {
 		if (!window.localStorage[this.STORAGE_KEY]) return false;
 		const data: IYandexUserData[] = JSON.parse(window.localStorage[this.STORAGE_KEY]);
-		const userData = data.find((x) => x.name == name);
+		const userData = data.find((x) => x.name == login);
 		if (!userData) return false;
 		return !!userData;
 	}
@@ -30,16 +30,26 @@ export class SmYandexAuthService {
 		return data.map((x) => x.name);
 	}
 
-	getTokenByAccount(accountName: string): string {
+	getTokenByAccount(login: string): string {
 		const data: IYandexUserData[] = JSON.parse(window.localStorage[this.STORAGE_KEY]);
-		const userData = data.find((x) => x.name == accountName);
+		const userData = data.find((x) => x.name == login);
 		if (!userData) return '';
 
 		if (userData.authData.expirationDate < Date.now()) {
-			alert('time');
+			window.localStorage['yandexFlow'] = '1';
+			this.exchangeRefreshTokenForToken(login).then(response => {
+				return userData.authData.access_token;
+			});
 		}
 
 		return userData.authData.access_token;
+	}
+
+	getRefreshToken(login: string): string {
+		const data: IYandexUserData[] = JSON.parse(window.localStorage[this.STORAGE_KEY]);
+		const userData = data.find((x) => x.name == login);
+		if (!userData) return '';
+		return userData.authData.refresh_token;
 	}
 
 	// get accessToken(): string {
@@ -61,12 +71,6 @@ export class SmYandexAuthService {
 
 	// 	return json.access_token;
 	// }
-
-	get refreshToken(): string {
-		const data = window.localStorage[this.STORAGE_KEY];
-		const json: IYandexAuthData = JSON.parse(data);
-		return json.refresh_token;
-	}
 
 	authorize(): void {
 		var url = `${this.AUTH_URL}/authorize?response_type=code&client_id=${this.APP_ID}&force_confirm=true`;
@@ -99,28 +103,38 @@ export class SmYandexAuthService {
 					usersArray = usersArray.filter((x) => x.name !== userData.name);
 					usersArray.push(userData);
 					window.localStorage[this.STORAGE_KEY] = JSON.stringify(usersArray);
-
 					window.localStorage['yandexFlow'] = '0';
 				});
 			});
 		});
 	}
 
-	exchangeRefreshTokenForToken(): void {
+	exchangeRefreshTokenForToken(login: string) {
 		let body = new URLSearchParams();
 		body.set('grant_type', 'refresh_token');
-		body.set('refresh_token', this.refreshToken);
+		body.set('refresh_token', this.getRefreshToken(login));
 		body.set('client_id', '1443711d95874286bfb64baa41c29459');
 		body.set('client_secret', '6e308b0e1a6d45b1abfad1096c35064c');
 
-		fetch('https://oauth.yandex.ru/token', {
+		return fetch('https://oauth.yandex.ru/token', {
 			body: body,
 			method: 'POST'
 		}).then((res) => {
 			res.json().then((tokenResponse: IYandexAuthData) => {
 				const expirationDate = Date.now() + tokenResponse.expires_in;
 				tokenResponse.expirationDate = expirationDate;
-				localStorage[this.STORAGE_KEY] = JSON.stringify(tokenResponse);
+				const userData: IYandexUserData = {
+					name: login,
+					authData: tokenResponse
+				};
+
+				let usersArray: IYandexUserData[];
+				if (!window.localStorage[this.STORAGE_KEY]) usersArray = [];
+				else usersArray = JSON.parse(window.localStorage[this.STORAGE_KEY]);
+				usersArray = usersArray.filter((x) => x.name !== userData.name);
+				usersArray.push(userData);
+				window.localStorage[this.STORAGE_KEY] = JSON.stringify(usersArray);
+				window.localStorage['yandexFlow'] = '0';
 			});
 		});
 	}
