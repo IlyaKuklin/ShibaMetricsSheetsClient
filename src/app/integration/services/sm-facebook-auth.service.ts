8d382585c2c261d336eae0b037f9680bd81f2a39
Ilya Kuklin <ilyakuklin43@gmail.com>
2021-05-06 21:37:08 +0300

    fb flow

--- src/app/integration/services/sm-facebook-auth.service.ts
+++ src/app/integration/services/sm-facebook-auth.service.ts
@@ -8,43 +8,16 @@ import { FacebookApiService } from 'src/api/rest/api';
 export class SmFacebookAuthService {
 	constructor(private readonly facebookApiService: FacebookApiService) {}
 
+	private readonly STORAGE_KEY: string = 'fb_data';
+
 	authorize() {
 		var url = `https://www.facebook.com/v10.0/dialog/oauth?client_id=261566715709670&redirect_uri=https://localhost:4200/fb-verification/&scope=ads_read`;
 		window.open(url, 'auth', 'resizable,scrollbars,status,width=600,height=500');
 	}
 
-	/** Обменять код доступа на краткосрочный токен */
 	exchangeCodeForToken(code: string) {
 		const redirectUrl = `${window.location.origin}/fb-verification/`;
 		return this.facebookApiService.apiFacebookExchangeCodeForTokenGet(code, redirectUrl);
-
-		// const url = 'https://graph.facebook.com/v10.0/oauth/access_token?';
-		// const params = new URLSearchParams({
-		// 	client_id: '261566715709670',
-		// 	redirect_uri: 'https://localhost:4200/fb-verification/',
-		// 	client_secret: '3654071a87f66a4427e13e77eb423b6e',
-		// 	code: code
-		//});
-
-		// return fetch(url + params, {
-		// 	method: 'GET'
-		// });
-	}
-
-	/** Обменять краткосрочный токен на долгосрочный токен*/
-	exchangeTokenForLongLivedToken(token: string) {
-		const url = 'https://graph.facebook.com/v10.0/oauth/access_token?';
-		const params = new URLSearchParams({
-			client_id: '261566715709670',
-			redirect_uri: 'https://localhost:4200/fb-verification/',
-			client_secret: '3654071a87f66a4427e13e77eb423b6e',
-			grant_type: 'fb_exchange_token',
-			fb_exchange_token: token
-		});
-
-		return fetch(url + params, {
-			method: 'GET'
-		});
 	}
 }
 
